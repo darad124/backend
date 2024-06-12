1c06db159bbaaa065cd8976cd747484ef027875b
darad124 <daryjoe765@gmail.com>
2024-06-12 03:35:45 +0100

    Include SQLite database with latest migrations

--- electioneer/election_system/settings.py
+++ electioneer/election_system/settings.py
@@ -11,31 +11,25 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-16a!x1^c*0!a#ck3y@1x)d(8(oe7+qbo^0+k#)c9fdoayiy$mv'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-16a!x1^c*0!a#ck3y@1x)d(8(oe7+qbo^0+k#)c9fdoayiy$mv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '52.41.36.82',
-    '54.191.253.12',
-    '44.226.122.3',
-    'backend-5dou.onrender.com'
-]
-
-
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,backend-5dou.onrender.com').split(',')
 
 # Application definition
 
@@ -52,7 +46,6 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -63,7 +56,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # Add this line
 ]
-
 
 ROOT_URLCONF = 'election_system.urls'
 
@@ -85,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'election_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -95,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -115,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -126,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -144,19 +132,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
 
-# settings.py
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
+# Other settings
 BREVO_API_KEY = os.getenv('BREVO_API_KEY')
 ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
-
-# Other settings...
-
-# electioneer/settings.py
-
 
 CACHES = {
     'default': {
@@ -166,5 +145,4 @@ CACHES = {
 }
 
 print(f"BREVO_API_KEY: {BREVO_API_KEY}, ADMIN_EMAIL: {ADMIN_EMAIL}")
-
 
